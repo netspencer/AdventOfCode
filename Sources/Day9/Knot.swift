5f4aa7c9e2ce2fbd78e1b517a37e735b4848b1a9
Spencer Schoeben <spencer@netspencer.com>
2022-12-08 23:21:55 -0800

    refactor

--- Sources/Day9/Knot.swift
+++ Sources/Day9/Knot.swift
@@ -10,34 +10,16 @@ struct Knot: Hashable {
     
     mutating func move(direction: Direction) {
         switch direction {
-        case .up:
-            y += 1
-        case .down:
-            y -= 1
-        case .left:
-            x -= 1
-        case .right:
-            x += 1
+        case .up: y += 1
+        case .down: y -= 1
+        case .left: x -= 1
+        case .right: x += 1
         }
     }
     
     mutating func move(towards other: Knot) {
         guard !isAdjacent(other) else { return }
-        
-        let xMove = x < other.x ? 1 : -1
-        let yMove = y < other.y ? 1 : -1
-        
-        if x == other.x { // in same column
-            // move up/down
-            y += yMove
-        } else if y == other.y { // in same row
-            // move left/right
-            x += xMove
-        } else {
-            // move diagnoally
-            x += xMove
-            y += yMove
-        }
+        x = other.x - (other.x - x) / 2
+        y = other.y - (other.y - y) / 2
     }
 }
-
